69891fb2dfa30d57fd8a58606aa1f319324dc10b
NLNT <crundee54321@gmail.com>
2020-03-09 20:08:10 +0700

    Created & styled hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
 	theme: {
 		fontFamily: {
 			'inter': ['"Inter var"', ...defaultTheme.fontFamily.sans],
-		}
+		},
+		borderRadius: {
+      'xl': '4.5rem',
+    }
 	},
 	variants : {},
 	plugins  : []
